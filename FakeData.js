cc2acecb4c0a4e4bdf9022e9a2ca513ef4d27c3a
David Tunnell <davidtunnell9@gmail.com>
2021-10-23 12:50:59 -0500

    1st commit

---
+++ FakeData.js
@@ -0,0 +1,19 @@
+let users = [
+    {
+        name: "Pedro",
+        age: 19,
+        married: false,
+    },
+    {
+        name: "Alice",
+        age: 18,
+        married: true,
+    },
+    {
+        name: "Jack",
+        age: 22,
+        married: false,
+    },
+];
+
+module.exports = users;
---
+++ README.md
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=xCzm1bbOpfw&ab_channel=PedroTech
+
+Learn Apollo-Server-Express! In this video I will go over the basics of graphql using the GraphQL library Apollo Server. I have other tutorials on GraphQL if you want to check out!
+
+Extension: Apollo GraphQL
